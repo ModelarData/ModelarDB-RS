3ea63cf542f86cf389b3a80b6b53cf2839d375bc
Soeren Kejser Jensen <devel@kejserjensen.dk>
2022-07-04 22:35:58 +0200

    Add the PMC-Mean model type

--- server/src/models/pmcmean.rs
+++ server/src/models/pmcmean.rs
@@ -17,6 +17,144 @@ use std::convert::TryInto;
 
 use datafusion::arrow::array::{Float32Builder, Int32Builder, TimestampMillisecondBuilder};
 
+/** Public Type **/
+struct PMCMeanFitState {
+    error: f32,
+    size: u32,
+    min: f32,
+    max: f32,
+    sum: f64,
+}
+
+impl PMCMeanFitState {
+    fn new(error: f32) -> Self {
+        Self {
+            error,
+            size: 0,
+            min: f32::NAN,
+            max: f32::NAN,
+            sum: 0.0,
+        }
+    }
+
+    fn fit(&mut self, value: f32) -> bool {
+        let next_sum = self.sum + value as f64;
+        let average = next_sum / (self.size + 1) as f64;
+
+        let next_min = f32::min(self.min, value);
+        let next_max = f32::max(self.max, value);
+        if PMCMeanFitState::within_percentage_error_bound(self.error, average as f32, next_min)
+            || PMCMeanFitState::within_percentage_error_bound(self.error, average as f32, next_max)
+        {
+            self.min = next_min;
+            self.max = next_max;
+            self.sum = next_sum;
+            self.size += 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn get(&self) -> f32 {
+        (self.sum / self.size as f64) as f32
+    }
+
+    fn within_percentage_error_bound(error: f32, real: f32, approximation: f32) -> bool {
+	//Necessary as the method would return NaN if approximation and real are zero
+	if approximation == real || (real.is_nan() && approximation.is_nan()) {
+	    true
+	} else {
+	    let difference = real - approximation;
+	    let result = f32::abs(difference / real);
+	    (result * 100.0) <= error
+	}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use proptest::num::f32;
+    use proptest::{prop_assert, prop_assume, proptest};
+
+    pub static SEQUENCE_OF_NAN: [f32; 5] = [f32::NAN, f32::NAN, f32::NAN, f32::NAN, f32::NAN];
+
+    pub static SEQUENCE_OF_POSITIVE_INFINITY: [f32; 5] = [
+        f32::INFINITY,
+        f32::INFINITY,
+        f32::INFINITY,
+        f32::INFINITY,
+        f32::INFINITY,
+    ];
+
+    pub static SEQUENCE_OF_NEGATIVE_INFINITY: [f32; 5] = [
+        f32::NEG_INFINITY,
+        f32::NEG_INFINITY,
+        f32::NEG_INFINITY,
+        f32::NEG_INFINITY,
+        f32::NEG_INFINITY,
+    ];
+
+    #[test]
+    fn test_fit_sequence_of_nans() {
+        let mut state = PMCMeanFitState::new(0.0);
+        for value in SEQUENCE_OF_NAN {
+            assert!(state.fit(value));
+        }
+        assert!(state.get().is_nan());
+    }
+
+    #[test]
+    fn test_fit_sequence_of_positive_infinity() {
+        let mut state = PMCMeanFitState::new(0.0);
+        for value in SEQUENCE_OF_POSITIVE_INFINITY {
+            assert!(state.fit(value));
+        }
+        assert!(state.get() == f32::INFINITY);
+    }
+
+    #[test]
+    fn test_fit_sequence_of_negative_infinity() {
+        let mut state = PMCMeanFitState::new(0.0);
+        for value in SEQUENCE_OF_NEGATIVE_INFINITY {
+            assert!(state.fit(value));
+        }
+        assert!(state.get() == f32::NEG_INFINITY);
+    }
+
+    proptest! {
+        #[test]
+        fn test_fit_one_value_always_succeeds(value in f32::ANY) {
+            prop_assert!(PMCMeanFitState::new(0.0).fit(value));
+        }
+
+        #[test]
+        fn test_fit_nan_other_always_fails(value in f32::ANY) {
+            prop_assume!( ! value.is_nan());
+            let mut state = PMCMeanFitState::new(0.0);
+            assert!(state.fit(f32::NAN));
+            prop_assert!( ! state.fit(value));
+        }
+
+        #[test]
+        fn test_fit_positive_infinity_other_always_fails(value in f32::ANY) {
+            prop_assume!(value != f32::INFINITY);
+            let mut state = PMCMeanFitState::new(0.0);
+            assert!(state.fit(f32::INFINITY));
+            prop_assert!( ! state.fit(value));
+        }
+
+        #[test]
+        fn test_fit_negative_infinity_other_always_fails(value in f32::ANY) {
+            prop_assume!(value != f32::NEG_INFINITY);
+            let mut state = PMCMeanFitState::new(0.0);
+            assert!(state.fit(f32::NEG_INFINITY));
+            prop_assert!( ! state.fit(value));
+        }
+    }
+}
+
 /** Public Functions **/
 pub fn min_max(
     gid: i32,
