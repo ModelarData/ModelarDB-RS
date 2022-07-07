73e8299dbf06c9b220fc69c4de1719abe4d588fc
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2022-07-07 12:04:36 +0200

    Added basic fields for storage engine and general documentation.

--- server/src/storage.rs
+++ server/src/storage.rs
@@ -1,6 +1,11 @@
 //! Module containing support for formatting uncompressed data, storing uncompressed data both
 //! in-memory and in a parquet file data buffer and storing compressed data.
 //!
+//! The interface for interacting with the storage engine is the public "StorageEngine" struct that
+//! exposes the public "new" and "insert_data" functions. The storage engine should always be
+//! initialized with "StorageEngine::new()". Using "insert_data", sensor data can be inserted into
+//! the engine where it is further processed to reach the final compressed state.
+//!
 /* Copyright 2021 The MiniModelarDB Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,3 +20,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+use std::collections::HashMap;
+use datafusion::arrow::array::PrimitiveBuilder;
+use datafusion::arrow::datatypes::Float32Type;
+use datafusion::parquet::data_type::Int64Type;
+
+/// Storage engine struct responsible for keeping track of all uncompressed data and invoking the
+/// compressor to move the uncompressed data into persistent model-based storage. The fields should
+/// not be directly modified and are therefore only changed when using "insert_data".
+///
+/// # Fields
+/// * `data` - The in-memory representation of uncompressed data. A hash map from the time series ID
+/// to a tuple with the following format: (timestamp_array_builder, value_array_builder, \[metadata]).
+/// * `data_buffer` - Hash map from the time series ID to the path of the parquet file buffer.
+pub struct StorageEngine {
+    data: HashMap<String, (PrimitiveBuilder<Int64Type>, PrimitiveBuilder<Float32Type>, [&'static str])>,
+    data_buffer: HashMap<String, String>
+}
