f8c19a9f1f4d5de84dd5dd18111d376f357c52a7
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2022-07-15 06:33:45 +0200

    Renamed "time_series" to "segment" to better reflect that it is finite

--- server/src/storage/mod.rs
+++ server/src/storage/mod.rs
@@ -17,13 +17,14 @@
 //! in-memory and in a parquet file data buffer, and storing compressed data.
 
 mod data_point;
-mod time_series;
+mod segment;
 
-use std::collections::{HashMap, VecDeque};
+use crate::storage::data_point::DataPoint;
+use crate::storage::segment::SegmentBuilder;
 use datafusion::arrow::record_batch::RecordBatch;
 use paho_mqtt::Message;
-use crate::storage::data_point::DataPoint;
-use crate::storage::time_series::{TimeSeriesBuilder};
+use std::collections::{HashMap};
+use std::collections::vec_deque::VecDeque;
 
 type Timestamp = i64;
 type Value = f32;
@@ -34,10 +35,10 @@ const INITIAL_BUILDER_CAPACITY: usize = 50;
 /// Keeping track of all uncompressed data, either in memory or in a file buffer. The fields should
 /// not be directly modified and are therefore only changed when using "insert_data".
 pub struct StorageEngine {
-    /// The uncompressed time series while they are being built.
-    data: HashMap<String, TimeSeriesBuilder>,
-    /// Prioritized queue of finished time series that are ready for compression.
-    compression_queue: VecDeque<TimeSeriesBuilder>
+    /// The uncompressed segments while they are being built.
+    data: HashMap<String, SegmentBuilder>,
+    /// Prioritized queue of finished segments that are ready for compression.
+    compression_queue: VecDeque<SegmentBuilder>,
 }
 
 impl StorageEngine {
@@ -45,7 +46,7 @@ impl StorageEngine {
         StorageEngine {
             // TODO: Maybe create with estimated capacity to avoid reallocation.
             data: HashMap::new(),
-            compression_queue: VecDeque::new()
+            compression_queue: VecDeque::new(),
         }
     }
 
@@ -56,29 +57,35 @@ impl StorageEngine {
 
         println!("Inserting data point {:?} into key '{}'.", data_point, key);
 
-        if let Some(time_series) = self.data.get_mut(&*key) {
-            println!("Found existing time series with key '{}'.", key);
+        if let Some(segment) = self.data.get_mut(&*key) {
+            println!("Found existing segment with key '{}'.", key);
 
-            time_series.insert_data(&data_point);
+            segment.insert_data(&data_point);
 
-            if time_series.is_full() {
-                println!("Time series is full, moving it to the compression queue.");
+            if segment.is_full() {
+                println!("Segment is full, moving it to the compression queue.");
 
-                let finished_time_series = self.data.remove(&*key).unwrap();
-                self.compression_queue.push_back(finished_time_series);
+                let finished_segment = self.data.remove(&*key).unwrap();
+                self.compression_queue.push_back(finished_segment);
             }
         } else {
-            println!("Could not find time series with key '{}'. Creating time series.", key);
+            println!("Could not find segment with key '{}'. Creating segment.", key);
 
-            let mut time_series = TimeSeriesBuilder::new(&data_point);
-            time_series.insert_data(&data_point);
+            let mut segment = SegmentBuilder::new(&data_point);
+            segment.insert_data(&data_point);
 
-            self.data.insert(key, time_series);
+            self.data.insert(key, segment);
         }
     }
 
-    /// If possible, return the oldest finished time series from the compression queue.
-    pub fn get_finished_time_series(&mut self) -> Option<TimeSeriesBuilder> {
+    /// If possible, return the oldest finished segment from the compression queue.
+    pub fn get_finished_segment(&mut self) -> Option<SegmentBuilder> {
         self.compression_queue.pop_front()
     }
 }
+
+// TODO: Test for inserting a message into a segment that does not exist.
+// TODO: Test for inserting a message into a segment that does exist.
+// TODO: Test for inserting a message into a segment that becomes full.
+// TODO: Test for getting a finished segment while there is none.
+// TODO: Test for getting a finished segment when they are at least one.
