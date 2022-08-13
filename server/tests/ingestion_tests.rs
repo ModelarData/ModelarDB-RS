6f0a8a404ac9a9b72818d91b501aba1d3f8088b2
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2022-08-13 11:21:16 +0200

    Added a test utility function to generate a random message

--- server/tests/ingestion_tests.rs
+++ server/tests/ingestion_tests.rs
@@ -1,3 +1,14 @@
+use std::sync::Arc;
+use std::time::{SystemTime, UNIX_EPOCH};
+use datafusion::arrow::array;
+
+use rand::Rng;
+use datafusion::arrow::array::{Float32Array, PrimitiveArray, StringArray, TimestampMillisecondArray};
+use datafusion::arrow::datatypes::{DataType, Field, Schema, TimestampMillisecondType};
+use datafusion::arrow::datatypes::TimeUnit::Millisecond;
+use datafusion::arrow::record_batch::RecordBatch;
+use proptest::collection::vec;
+
 #[test]
 fn test_ingest_message_into_storage_engine() {
     // TODO: Send a single message to do_put.
@@ -51,4 +62,35 @@ fn test_can_query_ingested_uncompressed_and_compressed_data() {
     // TODO: Send BUILDER_CAPACITY + 1 messages from the same time series to do_put.
     // TODO: Use the query engine to query from the time series the messages belong to.
     // TODO: Ensure that the uncompressed message is part of the query result.
+}
+
+/// Generate `count` random messages for the time series referenced by `key` and send it to the
+/// ModelarDB server with Arrow Flight through the `do_put()` endpoint.
+fn send_messages_to_server(key: String) {
+
+}
+
+/// Generate a [`RecordBatch`] with the current timestamp, a random value, and `key`.
+fn generate_random_message(key: String) -> RecordBatch {
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_micros() as i64;
+
+    let value = rand::thread_rng().gen_range(0..100) as f32;
+
+    let message_schema = Schema::new(vec![
+        Field::new("timestamp", DataType::Timestamp(Millisecond, None), false),
+        Field::new("value", DataType::Float32, false),
+        Field::new("key",DataType::Utf8, false),
+    ]);
+
+    RecordBatch::try_new(
+        Arc::new(message_schema),
+        vec![
+            Arc::new(TimestampMillisecondArray::from(vec![timestamp])),
+            Arc::new(Float32Array::from(vec![value])),
+            Arc::new(StringArray::from(vec![key])),
+        ]
+    ).unwrap()
 }
