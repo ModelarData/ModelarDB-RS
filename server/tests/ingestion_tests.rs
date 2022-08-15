5890349e8ba26132e474e3f04aa36821104d1e8e
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2022-08-15 09:49:24 +0200

    Added general structure for setting up integration test

--- server/tests/ingestion_tests.rs
+++ server/tests/ingestion_tests.rs
@@ -1,17 +1,24 @@
 use std::sync::Arc;
 use std::time::{SystemTime, UNIX_EPOCH};
-use datafusion::arrow::array;
 
 use rand::Rng;
+use arrow_flight::flight_service_client::FlightServiceClient;
+use assert_cmd::Command;
+use datafusion::arrow::array;
 use datafusion::arrow::array::{Float32Array, PrimitiveArray, StringArray, TimestampMillisecondArray};
 use datafusion::arrow::datatypes::{DataType, Field, Schema, TimestampMillisecondType};
 use datafusion::arrow::datatypes::TimeUnit::Millisecond;
 use datafusion::arrow::record_batch::RecordBatch;
-use proptest::collection::vec;
+use futures::executor::block_on;
+use tonic::transport::Channel;
 
 #[test]
 fn test_ingest_message_into_storage_engine() {
+    start_arrow_flight_server();
+    let client = create_flight_service_client();
+
     // TODO: Send a single message to do_put.
+    send_messages_to_arrow_flight_server(client,1, "key".to_owned())
     // TODO: Assert that a new segment has been created in the storage engine.
 }
 
@@ -64,10 +71,28 @@ fn test_can_query_ingested_uncompressed_and_compressed_data() {
     // TODO: Ensure that the uncompressed message is part of the query result.
 }
 
+fn start_arrow_flight_server() {
+    // TODO: Call the "mdbd" binary to start the server.
+    // TODO: It might be necessary to create a temporary data folder.
+}
+
+fn create_flight_service_client() -> FlightServiceClient<Channel> {
+    let address = format!("grpc://127.0.0.1:9999");
+
+    block_on(async {
+        let client = FlightServiceClient::connect(address).await?;
+        Ok(client)
+    }).unwrap()
+}
+
 /// Generate `count` random messages for the time series referenced by `key` and send it to the
 /// ModelarDB server with Arrow Flight through the `do_put()` endpoint.
-fn send_messages_to_server(key: String) {
+fn send_messages_to_arrow_flight_server(client: FlightServiceClient<Channel>, count: usize, key: String) {
+    for _ in 0..count {
+        let message = generate_random_message(key.clone());
 
+        // TODO: Send the message.
+    }
 }
 
 /// Generate a [`RecordBatch`] with the current timestamp, a random value, and `key`.
