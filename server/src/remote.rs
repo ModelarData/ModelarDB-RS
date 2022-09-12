fd916031510626ba5b40f19f05470bef4ff5b6a5
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2022-09-12 11:47:54 +0200

    Moved some free functions to be associated functions on the flight service handler

--- server/src/remote.rs
+++ server/src/remote.rs
@@ -23,7 +23,7 @@ use std::error::Error;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::pin::Pin;
-use std::sync::{Arc, RwLockWriteGuard};
+use std::sync::Arc;
 use std::{mem, str};
 
 use arrow_flight::flight_service_server::{FlightService, FlightServiceServer};
@@ -46,7 +46,7 @@ use tracing::{error, info};
 
 use crate::catalog::{NewModelTableMetadata, TableMetadata};
 use crate::storage::StorageEngine;
-use crate::{Catalog, Context};
+use crate::Context;
 
 /// Start an Apache Arrow Flight server on 0.0.0.0:`port` that pass `context` to
 /// the methods that process the requests through `FlightServiceHandler`.
@@ -155,6 +155,87 @@ impl FlightServiceHandler {
             let mut storage_engine = self.context.storage_engine.write().unwrap();
             storage_engine.insert_data(model_table.clone(), record_batch);
         }
+
+        Ok(())
+    }
+
+    /// Create a normal table and add it to the catalog. If the table already exists or if the
+    /// Apache Parquet file cannot be created, return [`Status`] error.
+    fn create_table(&self, table_name: String, schema: Schema, ) -> Result<(), Status> {
+        // Get the write lock for the catalog to ensure that multiple writes cannot happen at
+        // the same time. unwrap() is safe to use since write() only fails if the RwLock is poisoned.
+        let mut catalog = self.context.catalog.write().unwrap();
+
+        // If the table already exists, return an error.
+        let mut existing_tables = catalog.table_metadata.iter();
+        if existing_tables.any(|table| table.name == table_name) {
+            let message = format!("Table with name '{}' already exists.", table_name);
+            return Err(Status::already_exists(message));
+        }
+
+        let file_name = format!("{}.parquet", table_name);
+        let file_path = catalog.data_folder_path.join(file_name);
+
+        // Create an empty Apache Parquet file to save the schema.
+        let empty_batch = RecordBatch::new_empty(Arc::new(schema));
+        StorageEngine::write_batch_to_apache_parquet_file(empty_batch, file_path.as_path())
+            .map_err(|error| Status::invalid_argument(error.to_string()))?;
+
+        // Save the table metadata in the catalog.
+        let path_str = file_path.to_str().unwrap().to_string();
+        let new_table_metadata = TableMetadata {
+            name: table_name.clone(),
+            path: path_str,
+        };
+
+        catalog.table_metadata.push(new_table_metadata);
+        info!("Created table '{}'.", table_name);
+
+        Ok(())
+    }
+
+    /// Create a model table, add it to the metadata database tables, and add it to the catalog.
+    /// If the table already exists or if the metadata cannot be written to the metadata database,
+    /// return [`Status`] error.
+    fn create_model_table(
+        &self,
+        table_name: String,
+        schema: Schema,
+        tag_column_indices: Vec<u8>,
+        timestamp_column_index: u8,
+    ) -> Result<(), Status> {
+        // Get the write lock for the catalog to ensure that multiple writes cannot happen at
+        // the same time. unwrap() is safe to use since write() only fails if the RwLock is poisoned.
+        let mut catalog = self.context.catalog.write().unwrap();
+
+        // If the table already exists, return an error.
+        let mut existing_tables = catalog.new_model_table_metadata.iter();
+        if existing_tables.any(|table| table.name == table_name) {
+            let message = format!("Model table with name '{}' already exists.", table_name);
+            return Err(Status::already_exists(message));
+        }
+
+        let model_table_metadata = NewModelTableMetadata::try_new(
+            table_name.clone(),
+            schema.clone(),
+            tag_column_indices,
+            timestamp_column_index,
+        ).map_err(|error| Status::invalid_argument(error.to_string()))?;
+
+        // Convert the schema to bytes so it can be saved as a BLOB in the metadata database.
+        let options = IpcWriteOptions::default();
+        let schema_as_ipc = SchemaAsIpc::new(&schema, &options);
+        let ipc_message: IpcMessage = schema_as_ipc
+            .try_into()
+            .map_err(|error: ArrowError| Status::internal(error.to_string()))?;
+
+        // Persist the new model table to the metadata database.
+        let database_path = catalog.data_folder_path.join("metadata.sqlite3");
+        save_model_table_to_database(database_path, &model_table_metadata, ipc_message.0)
+            .map_err(|error| Status::internal(error.to_string()))?;
+
+        info!("Created model table '{}'.", table_name);
+        catalog.new_model_table_metadata.push(model_table_metadata);
 
         Ok(())
     }
@@ -387,12 +468,8 @@ impl FlightService for FlightServiceHandler {
             let schema = Schema::try_from(ipc_message)
                 .map_err(|error| Status::invalid_argument(error.to_string()))?;
 
-            // Get the write lock for the catalog to ensure that multiple writes cannot happen at
-            // the same time. unwrap() is safe to use since write() only fails if the RwLock is poisoned.
-            let catalog = self.context.catalog.write().unwrap();
-
             if action.r#type == "CreateTable" {
-                create_table(catalog, table_name.to_owned(), schema)?;
+                self.create_table(table_name.to_owned(), schema)?;
             } else {
                 // Extract the tag column indices from the action body. Note that since we assume
                 // each tag column index is one byte, we directly use the slice of bytes as the list
@@ -402,8 +479,7 @@ impl FlightService for FlightServiceHandler {
                 // Extract the timestamp column index from the action body.
                 let (timestamp_index, _offset_data) = extract_argument_bytes(offset_data);
 
-                create_model_table(
-                    catalog,
+                self.create_model_table(
                     table_name.to_owned(),
                     schema,
                     tag_indices.to_vec(),
@@ -453,83 +529,6 @@ fn extract_argument_bytes(data: &[u8]) -> (&[u8], &[u8]) {
     let remaining_bytes = &data[(size + 2)..];
 
     (argument_bytes, remaining_bytes)
-}
-
-/// Create a normal table and add it to the catalog. If the table already exists or if the Apache
-/// Parquet file cannot be created, return [`Status`] error.
-fn create_table(
-    mut catalog: RwLockWriteGuard<Catalog>,
-    table_name: String,
-    schema: Schema,
-) -> Result<(), Status> {
-    // If the table already exists, return an error.
-    let mut existing_tables = catalog.table_metadata.iter();
-    if existing_tables.any(|table| table.name == table_name) {
-        let message = format!("Table with name '{}' already exists.", table_name);
-        return Err(Status::already_exists(message));
-    }
-
-    let file_name = format!("{}.parquet", table_name);
-    let file_path = catalog.data_folder_path.join(file_name);
-
-    // Create an empty Apache Parquet file to save the schema.
-    let empty_batch = RecordBatch::new_empty(Arc::new(schema));
-    StorageEngine::write_batch_to_apache_parquet_file(empty_batch, file_path.as_path())
-        .map_err(|error| Status::invalid_argument(error.to_string()))?;
-
-    // Save the table metadata in the catalog.
-    let path_str = file_path.to_str().unwrap().to_string();
-    let new_table_metadata = TableMetadata {
-        name: table_name.clone(),
-        path: path_str,
-    };
-
-    info!("Created table '{}'.", table_name);
-    catalog.table_metadata.push(new_table_metadata);
-
-    Ok(())
-}
-
-/// Create a model table, add it to the metadata database tables, and add it to the catalog. If the
-/// table already exists or if the metadata cannot be written to the metadata database, return [`Status`] error.
-fn create_model_table(
-    mut catalog: RwLockWriteGuard<Catalog>,
-    table_name: String,
-    schema: Schema,
-    tag_column_indices: Vec<u8>,
-    timestamp_column_index: u8,
-) -> Result<(), Status> {
-    // If the table already exists, return an error.
-    let mut existing_tables = catalog.new_model_table_metadata.iter();
-    if existing_tables.any(|table| table.name == table_name) {
-        let message = format!("Model table with name '{}' already exists.", table_name);
-        return Err(Status::already_exists(message));
-    }
-
-    let model_table_metadata = NewModelTableMetadata::try_new(
-        table_name.clone(),
-        schema.clone(),
-        tag_column_indices,
-        timestamp_column_index,
-    )
-    .map_err(|error| Status::invalid_argument(error.to_string()))?;
-
-    // Convert the schema to bytes so it can be saved as a BLOB in the metadata database.
-    let options = IpcWriteOptions::default();
-    let schema_as_ipc = SchemaAsIpc::new(&schema, &options);
-    let ipc_message: IpcMessage = schema_as_ipc
-        .try_into()
-        .map_err(|error: ArrowError| Status::internal(error.to_string()))?;
-
-    // Persist the new model table to the metadata database.
-    let database_path = catalog.data_folder_path.join("metadata.sqlite3");
-    save_model_table_to_database(database_path, &model_table_metadata, ipc_message.0)
-        .map_err(|error| Status::internal(error.to_string()))?;
-
-    info!("Created model table '{}'.", table_name);
-    catalog.new_model_table_metadata.push(model_table_metadata);
-
-    Ok(())
 }
 
 // TODO: Move this to the metadata component when it exists.
