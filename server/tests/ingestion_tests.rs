3d8917e7cc787cac59110f532a8cfe67bdeb76ab
agneborn98 <43062786+agneborn98@users.noreply.github.com>
2022-10-30 19:26:07 +0100

    Minor fixes

--- server/tests/ingestion_tests.rs
+++ server/tests/ingestion_tests.rs
@@ -1,17 +1,22 @@
 use std::{env, io};
 use std::borrow::Borrow;
+use std::collections::HashMap;
 use std::error::Error;
 
 use std::fs;
 use std::io::{BufRead, Write};
+use std::os::linux::process::PidFd;
 use std::path;
+use std::path::Path;
 use std::process;
+use std::process::Child;
 use std::sync::Arc;
+use std::sync::mpsc::channel;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use arrow_flight::flight_service_client::FlightServiceClient;
-use arrow_flight::{Action, FlightData, FlightDescriptor, FlightInfo, PutResult};
-use arrow_flight::utils::flight_data_from_arrow_batch;
+use arrow_flight::{Action, FlightData, FlightDescriptor, FlightInfo, PutResult, SchemaAsIpc};
+use arrow_flight::utils::{flight_data_from_arrow_batch, flight_data_to_arrow_batch};
 use assert_cmd::Command;
 use assert_cmd::output::OutputError;
 use datafusion::arrow::{array, ipc};
@@ -24,6 +29,9 @@ use datafusion::arrow::error::ArrowError;
 use datafusion::arrow::record_batch::RecordBatch;
 use futures::executor::block_on;
 use futures::stream;
+use nix::unistd::Pid;
+use nix::sys::signal::{self, Signal};
+use libc;
 use log::error;
 use prost::Message;
 use rand::Rng;
@@ -37,28 +45,41 @@ pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 #[test]
 fn test_ingest_message_into_storage_engine() {
-    //create_directory().expect("Failed to create directory");
-
-    //let mut flight_server = start_arrow_flight_server();
-
+    create_directory();
 
     //let mut f = fs::File::create("tests/ArrowFlight.log").expect("Failed to create log file");
     //io::copy(&mut flight_server.stdout.unwrap(), &mut f).expect("Failed to write to stdout");
+
+    let message = generate_random_message("location".to_string());
+
+    let mut flight_server = start_arrow_flight_server();
 
     if let Ok(rt) = Runtime::new() {
         let address = ("127.0.0.1".to_string());
         match create_flight_service_client(&rt, &address, 9999) {
             Ok(mut fsc) => {
 
+                let tag = "location".to_string();
+
                 let _result_create_table = create_table_in_folder(&mut fsc);
 
-                let _result = send_messages_to_arrow_flight_server(&mut fsc, 1, "location".to_owned());
+                let options = ipc::writer::IpcWriteOptions::default();
 
-                // Print error.
-               // match result {
-                  //  Ok(_) => (),
-                 //   Err(message) => eprintln!("{}", message),
-                //};
+                let flight_descriptor = FlightDescriptor::new_path(vec![String::from("data")]);
+
+                let mut flight_data_vec = vec![FlightData{
+                    flight_descriptor: Some(flight_descriptor),
+                    data_header: vec![],
+                    app_metadata: vec![],
+                    data_body: vec![]
+                }];
+
+                let (_ , flight_data) = flight_data_from_arrow_batch(&message, &options);
+
+                flight_data_vec.push(flight_data);
+
+                let _result = send_messages_to_arrow_flight_server(&mut fsc, flight_data_vec);
+
             }
             Err(message) => eprintln!("error: cannot connect to {} due to a {}", address, message),
         }
@@ -67,7 +88,29 @@ fn test_ingest_message_into_storage_engine() {
     }
     // TODO: Assert that a new segment has been created in the storage engine.
 
-    //remove_directory().expect("Failed to remove directory");
+    let mut flight_server_2 = start_arrow_flight_server();
+
+    if let Ok(rt) = Runtime::new() {
+        let address = ("127.0.0.1".to_string());
+        match create_flight_service_client(&rt, &address, 9999) {
+            Ok(mut fsc) => {
+
+                let query_result = execute_query(&mut fsc,"SELECT * FROM data");
+
+                let result = query_result.expect("Could not execute query.");
+
+
+                assert_eq!(result[0], message);
+            }
+            Err(message) => eprintln!("error: cannot connect to {} due to a {}", address, message),
+        }
+    } else {
+        eprintln!("error: unable to initialize run-time");
+    }
+
+    flight_server_2.kill().expect("Could not kill process.");
+
+    remove_directory().expect("Failed to remove directory");
 }
 
 #[test]
@@ -145,9 +188,11 @@ fn start_binary(binary: &str) -> process::Command {
     process::Command::new(path.into_os_string())
 }
 
-fn create_directory() -> std::io::Result<()> {
-    fs::create_dir("tests/data")?;
-    Ok(())
+fn create_directory() {
+
+    if !Path::new("tests/data").try_exists().unwrap(){
+        fs::create_dir("tests/data").expect("Could not create directory");
+    }
 }
 
 fn remove_directory() -> std::io::Result<()> {
@@ -190,34 +235,19 @@ fn create_table_in_folder(mut client: &mut FlightServiceClient<Channel>) -> std:
     })
 }
 
-/// Generate `count` random messages for the time series referenced by `tag` and send it to the
-/// ModelarDB server with Arrow Flight through the `do_put()` endpoint.
+/// Send messages to the ModelarDB server with
+/// Arrow Flight through the `do_put()` endpoint.
 fn send_messages_to_arrow_flight_server(
     mut client: &mut FlightServiceClient<Channel>,
-    count: usize,
-    tag: String,
+    flight_data_vec: Vec<FlightData>
 ) -> std::result::Result<(), ()> {
 
-        let runtime = tokio::runtime::Runtime::new().expect("Failed to start runtime(send_messages_to_arrow_flight_server)");
-        let message = generate_random_message(tag.clone());
 
-        // TODO: Send the message.
-        let flight_descriptor = FlightDescriptor::new_path(vec![String::from("data")]);
-
-        let (_ , flight_data) = flight_data_from_arrow_batch(&message, &ipc::writer::IpcWriteOptions::default());
-
-        let mut flight_data_vec = vec![FlightData{
-            flight_descriptor: Some(flight_descriptor),
-            data_header: vec![],
-            app_metadata: vec![],
-            data_body: vec![]
-        }];
-
-        flight_data_vec.push(flight_data);
+        let runtime = Runtime::new().expect("Failed to start runtime(send_messages_to_arrow_flight_server)");
 
         runtime.block_on(async {
 
-            let flight_data_stream = stream::(&flight_data_vec);
+            let flight_data_stream = stream::iter(flight_data_vec);
 
             let mut _streaming = client.do_put(flight_data_stream)
                 .await;
@@ -250,6 +280,38 @@ fn generate_random_message(tag: String) -> RecordBatch {
         ],
     )
     .unwrap()
+}
+
+fn execute_query(
+    fsc: &mut FlightServiceClient<Channel>,
+    query: &str,
+) -> Result<Vec<RecordBatch>> {
+    let runtime = Runtime::new().expect("Failed to start runtime(execute_query)");
+    runtime.block_on(async {
+        //Execute query
+        let ticket_data = query.to_owned().into_bytes();
+        let ticket = arrow_flight::Ticket {
+            ticket: ticket_data,
+        };
+        let mut stream = fsc.do_get(ticket).await?.into_inner();
+
+        //Get schema of result set
+        let flight_data = stream
+            .message()
+            .await?
+            .ok_or("transport error: no messages received")?;
+        let schema = Arc::new(Schema::try_from(&flight_data)?);
+
+        //Get data in result set
+        let mut results = vec![];
+        while let Some(flight_data) = stream.message().await? {
+            let dictionaries_by_id = HashMap::new();
+            let record_batch =
+                flight_data_to_arrow_batch(&flight_data, schema.clone(), &dictionaries_by_id)?;
+            results.push(record_batch);
+        }
+        Ok(results)
+    })
 }
 
 pub fn status_to_arrow_error(status: tonic::Status) -> ArrowError {
