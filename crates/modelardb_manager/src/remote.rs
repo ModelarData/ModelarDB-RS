81eebbdb8b4e7eff077fbb5a4a01cb31bbcdf997
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-04-25 08:18:57 +0200

    Added structure managers Arrow Flight API

---
+++ crates/modelardb_manager/src/remote.rs
@@ -0,0 +1,138 @@
+/* Copyright 2023 The ModelarDB Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//! Implementation of a request handler for Apache Arrow Flight in the form of `FlightServiceHandler`.
+//! An Apache Arrow Flight server that process requests using `FlightServiceHandler` can be started
+//! with `start_arrow_flight_server()`.
+
+use std::error::Error;
+use std::pin::Pin;
+
+use arrow_flight::flight_service_server::FlightService;
+use arrow_flight::{
+    Action, ActionType, Criteria, Empty, FlightData, FlightDescriptor, FlightInfo,
+    HandshakeRequest, HandshakeResponse, PutResult, SchemaResult, Ticket,
+};
+use futures::Stream;
+use tonic::{Request, Response, Status, Streaming};
+
+/// TODO: Start an Apache Arrow Flight server on 0.0.0.0:`port`.
+pub fn start_apache_arrow_flight_server() -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+/// Handler for processing Apache Arrow Flight requests.
+/// [`FlightServiceHandler`] is based on the [Apache Arrow Flight examples]
+/// published under Apache2.
+///
+/// [Apache Arrow Flight examples]: https://github.com/apache/arrow-rs/blob/master/arrow-flight/examples
+struct FlightServiceHandler {}
+
+#[tonic::async_trait]
+impl FlightService for FlightServiceHandler {
+    type HandshakeStream =
+        Pin<Box<dyn Stream<Item = Result<HandshakeResponse, Status>> + Send + Sync + 'static>>;
+    type ListFlightsStream =
+        Pin<Box<dyn Stream<Item = Result<FlightInfo, Status>> + Send + Sync + 'static>>;
+    type DoGetStream =
+        Pin<Box<dyn Stream<Item = Result<FlightData, Status>> + Send + Sync + 'static>>;
+    type DoPutStream =
+        Pin<Box<dyn Stream<Item = Result<PutResult, Status>> + Send + Sync + 'static>>;
+    type DoExchangeStream =
+        Pin<Box<dyn Stream<Item = Result<FlightData, Status>> + Send + Sync + 'static>>;
+    type DoActionStream =
+        Pin<Box<dyn Stream<Item = Result<arrow_flight::Result, Status>> + Send + Sync + 'static>>;
+    type ListActionsStream =
+        Pin<Box<dyn Stream<Item = Result<ActionType, Status>> + Send + Sync + 'static>>;
+
+    /// Not implemented.
+    async fn handshake(
+        &self,
+        _request: Request<Streaming<HandshakeRequest>>,
+    ) -> Result<Response<Self::HandshakeStream>, Status> {
+        Err(Status::unimplemented("Not implemented."))
+    }
+
+    /// TODO: Provide the name of all tables in the catalog.
+    async fn list_flights(
+        &self,
+        _request: Request<Criteria>,
+    ) -> Result<Response<Self::ListFlightsStream>, Status> {
+        Err(Status::unimplemented("Not implemented."))
+    }
+
+    /// Not implemented.
+    async fn get_flight_info(
+        &self,
+        _request: Request<FlightDescriptor>,
+    ) -> Result<Response<FlightInfo>, Status> {
+        Err(Status::unimplemented("Not implemented."))
+    }
+
+    /// TODO: Provide the schema of a table in the catalog. The name of the table must
+    ///       be provided as the first element in `FlightDescriptor.path`.
+    async fn get_schema(
+        &self,
+        _request: Request<FlightDescriptor>,
+    ) -> Result<Response<SchemaResult>, Status> {
+        Err(Status::unimplemented("Not implemented."))
+    }
+
+    /// TODO: Execute a SQL query provided in UTF-8 and return the schema of the query
+    ///       result followed by the query result.
+    async fn do_get(
+        &self,
+        _request: Request<Ticket>,
+    ) -> Result<Response<Self::DoGetStream>, Status> {
+        Err(Status::unimplemented("Not implemented."))
+    }
+
+    /// TODO: Insert metadata such as tags into the metadata database.
+    async fn do_put(
+        &self,
+        _request: Request<Streaming<FlightData>>,
+    ) -> Result<Response<Self::DoPutStream>, Status> {
+        Err(Status::unimplemented("Not implemented."))
+    }
+
+    /// Not implemented.
+    async fn do_exchange(
+        &self,
+        _request: Request<Streaming<FlightData>>,
+    ) -> Result<Response<Self::DoExchangeStream>, Status> {
+        Err(Status::unimplemented("Not implemented."))
+    }
+
+    /// TODO: Perform a specific action based on the type of the action in `request`.
+    ///       The following actions should be supported:
+    ///       * `CommandStatementUpdate`
+    ///       * `UpdateRemoteObjectStore`
+    ///       * `RegisterEdge`
+    ///       * `RemoveEdge`
+    async fn do_action(
+        &self,
+        _request: Request<Action>,
+    ) -> Result<Response<Self::DoActionStream>, Status> {
+        Err(Status::unimplemented("Not implemented."))
+    }
+
+    /// TODO: Return all available actions, including both a name and a description for each action.
+    async fn list_actions(
+        &self,
+        _request: Request<Empty>,
+    ) -> Result<Response<Self::ListActionsStream>, Status> {
+        Err(Status::unimplemented("Not implemented."))
+    }
+}
