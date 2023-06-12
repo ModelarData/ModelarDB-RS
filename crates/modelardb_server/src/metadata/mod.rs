57cc6a41bcf1a85287d4bd92df2ae9443b5fa40c
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2023-06-12 15:59:45 +0200

    Removed configuration from current metadata manager

--- crates/modelardb_server/src/metadata/mod.rs
+++ crates/modelardb_server/src/metadata/mod.rs
@@ -13,14 +13,13 @@
  * limitations under the License.
  */
 
-//! Management of the metadata database which stores the system's configuration and the metadata
-//! required for the tables and model tables. Tables can store arbitrary data, while model tables
-//! can only store time series as segments containing metadata and models. At runtime, the location
-//! of the data for the tables and the model table metadata are stored in Apache Arrow DataFusion's
-//! catalog, while this module stores the system's configuration and a mapping from model table name
-//! and tag values to hashes. These hashes can be combined with the corresponding model table's
-//! field column indices to uniquely identify each univariate time series stored in the storage
-//! engine by a univariate id.
+//! Management of the metadata database which stores the metadata required for the tables and
+//! model tables. Tables can store arbitrary data, while model tables can only store time series as
+//! segments containing metadata and models. At runtime, the location of the data for the tables and
+//! the model table metadata are stored in Apache Arrow DataFusion's catalog, while this module stores
+//! a mapping from model table name and tag values to hashes. These hashes can be combined with the
+//! corresponding model table's field column indices to uniquely identify each univariate time series
+//! stored in the storage engine by a univariate id.
 
 pub mod model_table_metadata;
 
@@ -51,16 +50,15 @@ use crate::metadata::model_table_metadata::ModelTableMetadata;
 use crate::parser;
 use crate::query::ModelTable;
 use crate::storage::COMPRESSED_DATA_FOLDER;
-use crate::{Context, ServerMode};
+use crate::Context;
 
 use self::model_table_metadata::GeneratedColumn;
 
 /// Name used for the file containing the SQLite database storing the metadata.
 pub const METADATA_DATABASE_NAME: &str = "metadata.sqlite3";
 
-/// Store's the system's configuration and the metadata required for reading from and writing to the
-/// tables and model tables. The data that needs to be persisted are stored in the metadata
-/// database.
+/// Store's the metadata required for reading from and writing to the tables and model tables.
+/// The data that needs to be persisted are stored in the metadata database.
 #[derive(Clone)]
 pub struct MetadataManager {
     /// Folder for storing metadata and Apache Parquet files on the local file
@@ -71,18 +69,12 @@ pub struct MetadataManager {
     /// Cache of tag value hashes used to signify when to persist new unsaved
     /// tag combinations.
     tag_value_hashes: HashMap<String, u64>,
-    /// The mode of the server used to determine the behaviour when modifying the remote object store.
-    pub server_mode: ServerMode,
-    /// Amount of memory to reserve for storing uncompressed data buffers.
-    pub uncompressed_reserved_memory_in_bytes: usize,
-    /// Amount of memory to reserve for storing compressed data buffers.
-    pub compressed_reserved_memory_in_bytes: usize,
 }
 
 impl MetadataManager {
     /// Return [`MetadataManager`] if a pool of connections to the metadata database in
     /// `local_data_folder` can be made, otherwise [`Error`] is returned.
-    pub async fn try_new(local_data_folder: &Path, server_mode: ServerMode) -> Result<Self> {
+    pub async fn try_new(local_data_folder: &Path) -> Result<Self> {
         if !Self::is_path_a_data_folder(local_data_folder) {
             warn!("The data folder is not empty and does not contain data from ModelarDB");
         }
@@ -97,10 +89,6 @@ impl MetadataManager {
             local_data_folder: local_data_folder.to_path_buf(),
             metadata_database_pool: SqlitePool::connect_with(options).await?,
             tag_value_hashes: HashMap::new(),
-            server_mode,
-            // Default values for parameters.
-            uncompressed_reserved_memory_in_bytes: 512 * 1024 * 1024, // 512 MiB
-            compressed_reserved_memory_in_bytes: 512 * 1024 * 1024,   // 512 MiB
         };
 
         // Create the necessary tables in the metadata database.
