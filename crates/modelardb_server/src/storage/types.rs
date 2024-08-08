959f18899cafc09dafcc240afe417e07bd6f14ed
Søren Kejser Jensen <devel@kejserjensen.dk>
2024-08-08 13:01:55 +0200

    Update based on comments from @chrthomsen

--- crates/modelardb_server/src/storage/types.rs
+++ crates/modelardb_server/src/storage/types.rs
@@ -1,4 +1,4 @@
-/* Co, Sumpyright 2023 The ModelarDB Contributors
+/* Copyright 2023 The ModelarDB Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
