12b72459020b1ccf3f49d318c23c36690bfe9917
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2024-11-07 00:22:10 +0100

    Using MODEL_TABLE_NAME constant in ModelTableMetadata tests

--- crates/modelardb_common/src/metadata/model_table_metadata.rs
+++ crates/modelardb_common/src/metadata/model_table_metadata.rs
@@ -232,7 +232,7 @@ mod test {
         let (query_schema, error_bounds, generated_columns) =
             model_table_schema_error_bounds_and_generated_columns();
         let result = ModelTableMetadata::try_new(
-            "table_name".to_owned(),
+            test::MODEL_TABLE_NAME.to_owned(),
             query_schema,
             error_bounds,
             generated_columns,
@@ -291,7 +291,7 @@ mod test {
     /// Return metadata for a model table with one tag column and the timestamp column at index 1.
     fn create_simple_model_table_metadata(query_schema: Schema) -> Result<ModelTableMetadata> {
         ModelTableMetadata::try_new(
-            "table_name".to_owned(),
+            test::MODEL_TABLE_NAME.to_owned(),
             Arc::new(query_schema),
             vec![ErrorBound::try_new_absolute(ERROR_BOUND_ZERO).unwrap()],
             vec![None],
@@ -303,7 +303,7 @@ mod test {
         let (query_schema, _error_bounds, generated_columns) =
             model_table_schema_error_bounds_and_generated_columns();
         let result = ModelTableMetadata::try_new(
-            "table_name".to_owned(),
+            test::MODEL_TABLE_NAME.to_owned(),
             query_schema,
             vec![],
             generated_columns,
@@ -317,7 +317,7 @@ mod test {
         let (query_schema, error_bounds, _generated_columns) =
             model_table_schema_error_bounds_and_generated_columns();
         let result = ModelTableMetadata::try_new(
-            "table_name".to_owned(),
+            test::MODEL_TABLE_NAME.to_owned(),
             query_schema,
             error_bounds,
             vec![],
@@ -344,7 +344,7 @@ mod test {
         });
 
         let result = ModelTableMetadata::try_new(
-            "table_name".to_owned(),
+            test::MODEL_TABLE_NAME.to_owned(),
             query_schema,
             error_bounds,
             generated_columns,
@@ -393,9 +393,8 @@ mod test {
 
         let generated_columns = vec![None, None, None];
 
-        let table_name = "table_name".to_owned();
         let result = ModelTableMetadata::try_new(
-            table_name,
+            test::MODEL_TABLE_NAME.to_owned(),
             Arc::new(Schema::new(fields)),
             error_bounds,
             generated_columns,
