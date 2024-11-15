e83671b4b259a14d3b563f47b4fa4a74c9e5da63
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2024-11-15 16:46:59 +0100

    Moved optimizer module to storage crate

---
+++ crates/modelardb_storage/src/optimizer/mod.rs
@@ -0,0 +1,29 @@
+/* Copyright 2021 The ModelarDB Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//! Implementation of physical optimizer rules that rewrites the physical plans produced by Apache
+//! Arrow DataFusion to execute queries more efficiently directly on the segments with metadata and
+//! models stored in model tables.
+
+mod model_simple_aggregates;
+
+use std::sync::Arc;
+
+use model_simple_aggregates::ModelSimpleAggregatesPhysicalOptimizerRule;
+
+/// Return all physical optimizer rules. Currently, only simple aggregates are executed on segments.
+pub fn physical_optimizer_rules() -> Vec<Arc<ModelSimpleAggregatesPhysicalOptimizerRule>> {
+    vec![Arc::new(ModelSimpleAggregatesPhysicalOptimizerRule {})]
+}
