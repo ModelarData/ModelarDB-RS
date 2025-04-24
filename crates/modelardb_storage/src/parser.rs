c7a4aab0b71239ada165aac003f67c6dcee0cd90
Søren Kejser Jensen <devel@kejserjensen.dk>
2025-04-24 11:57:42 +0000

    Remove TableMetadataManager

--- crates/modelardb_storage/src/parser.rs
+++ crates/modelardb_storage/src/parser.rs
@@ -49,6 +49,7 @@ use sqlparser::parser::{Parser, ParserError};
 use sqlparser::tokenizer::{Span, Token};
 
 use crate::error::{ModelarDbStorageError, Result};
+use crate::time_series_table_metadata::{GeneratedColumn, TimeSeriesTableMetadata};
 
 /// A top-level statement (CREATE, INSERT, SELECT, TRUNCATE, DROP, etc.) that have been tokenized,
 /// parsed, and for which semantic checks have verified that it is compatible with ModelarDB.
