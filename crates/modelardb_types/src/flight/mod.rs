708cd9b59cb9abf81748479f485e9c045da51105
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-05-26 20:49:47 +0200

    Add function to encode error bounds in time series table metadata

--- crates/modelardb_types/src/flight/mod.rs
+++ crates/modelardb_types/src/flight/mod.rs
@@ -17,6 +17,7 @@ use arrow::datatypes::Schema;
 
 use crate::error::Result;
 use crate::functions::try_convert_schema_to_bytes;
+use crate::types::{ErrorBound, TimeSeriesTableMetadata};
 
 pub mod protocol {
     include!(concat!(env!("OUT_DIR"), "/modelardb.flight.protocol.rs"));
@@ -32,4 +33,41 @@ pub fn encode_normal_table_metadata(
         name: table_name.to_string(),
         schema: try_convert_schema_to_bytes(schema)?,
     })
+}
+
+/// Return a vector of serializable Protocol Buffer messages for the error bounds of
+/// `time_series_table_metadata`.
+fn encode_error_bounds(
+    time_series_table_metadata: &TimeSeriesTableMetadata,
+) -> Vec<protocol::create_tables_request::time_series_table_metadata::ErrorBound> {
+    // Since the time series table metadata does not include error bounds for the generated columns,
+    // lossless error bounds are added for each generated column.
+    let mut error_bounds_all =
+        Vec::with_capacity(time_series_table_metadata.query_schema.fields().len());
+
+    let lossless = protocol::create_tables_request::time_series_table_metadata::ErrorBound {
+        r#type: 0,
+        value: 0.0,
+    };
+
+    for field in time_series_table_metadata.query_schema.fields() {
+        if let Ok(field_index) = time_series_table_metadata.schema.index_of(field.name()) {
+            let (error_bound_type, value) =
+                match time_series_table_metadata.error_bounds[field_index] {
+                    ErrorBound::Absolute(value) => (0, value as f32),
+                    ErrorBound::Relative(value) => (1, value),
+                };
+
+            error_bounds_all.push(
+                protocol::create_tables_request::time_series_table_metadata::ErrorBound {
+                    r#type: error_bound_type,
+                    value,
+                },
+            );
+        } else {
+            error_bounds_all.push(lossless);
+        }
+    }
+
+    error_bounds_all
 }
