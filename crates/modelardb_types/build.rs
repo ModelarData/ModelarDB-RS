e8b7222965c6662edd6351be30ab1ef9aef65d18
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-06-13 19:48:54 +0200

    Use cargo::rerun-if-changed to avoid unnecessary rebuilds

--- crates/modelardb_types/build.rs
+++ crates/modelardb_types/build.rs
@@ -1,6 +1,9 @@
 use std::io::Result as StdIoResult;
 
 fn main() -> StdIoResult<()> {
+    // Tell Cargo to rerun the build script if the proto definition changes.
+    println!("cargo::rerun-if-changed=src/flight/protocol.proto");
+
     prost_build::compile_protos(&["src/flight/protocol.proto"], &["src/"])?;
     Ok(())
 }
