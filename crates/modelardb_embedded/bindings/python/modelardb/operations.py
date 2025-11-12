edd36e4794772e8ee8a02c65d790ea2f0827f035
Søren Kejser Jensen <devel@kejserjensen.dk>
2025-11-12 09:35:54 +0100

    Add pip package (#360)
    * Include python -m build dist folder in gitignore
    
    * Make the use of setuptools explicit
    
    * Support adding ModelarDB source files to package
    
    * Add extension for bdist_wheel
    
    * Build modelardb_embedded automatically
    
    * Explicitly check for build dependencies
    
    * Support loading modelardb_embedded most places
    
    * Format all Python files using ruff
    
    * Load correct library and support FreeBSD
    
    * Update based on comments from @Copilot
    
    * Update based on comments from @CGodiksen
    
    * Sort the .gitignore file
    
    * Update based on comments from @chrthomsen

--- crates/modelardb_embedded/bindings/python/modelardb/operations.py
+++ crates/modelardb_embedded/bindings/python/modelardb/operations.py
@@ -13,8 +13,11 @@
 # limitations under the License.
 
 import os
+import sys
 import platform
 import warnings
+import sysconfig
+import pathlib
 from datetime import datetime
 from enum import Enum
 from typing import Self
@@ -56,32 +59,43 @@ class Operations:
 
             :raises RuntimeError: If it is not executed on Linux, macOS, or Windows.
             """
-            script_folder = os.path.dirname(os.path.abspath(__file__))
+            # Attempt to load the library installed as part of the Python package.
+            library_folder = pathlib.Path(__file__).parent.parent.resolve()
 
-            # Select the library to use based on the operating system.
+            if sys.platform == "win32":
+                # SHLIB_SUFFIX is not set and .pyd is used by Rust.
+                library_name = "modelardb_embedded.pyd"
+            else:
+                library_name = "modelardb_embedded" + sysconfig.get_config_var(
+                    "SHLIB_SUFFIX"
+                )
+
+            library_path = library_folder / library_name
+            if library_path.exists():
+                return library_path
+
+            # Attempt to load the library compiled in the development repository.
+            repository_root = pathlib.Path(
+                __file__
+            ).parent.parent.parent.parent.parent.parent.resolve()
+            library_folder = repository_root / "target" / build
+
             match platform.system():
-                case "Linux":
-                    library_path = (
-                            script_folder
-                            + f"/../../../../../target/{build}/libmodelardb_embedded.so"
-                    )
+                case "Linux" | "FreeBSD":
+                    library_path = library_folder / "libmodelardb_embedded.so"
                 case "Darwin":
-                    library_path = (
-                            script_folder
-                            + f"/../../../../../target/{build}/libmodelardb_embedded.dylib"
-                    )
+                    library_path = library_folder / "libmodelardb_embedded.dylib"
                 case "Windows":
-                    library_path = (
-                            script_folder
-                            + f"\\..\\..\\..\\..\\..\\target\\{build}\\modelardb_embedded.dll"
-                    )
+                    library_path = library_folder / "modelardb_embedded.dll"
                 case _:
-                    raise RuntimeError("Only Linux, macOS, and Windows are supported.")
+                    raise RuntimeError("Only Linux, FreeBSD, macOS, and Windows are supported.")
 
-            if not os.path.isfile(library_path):
-                raise RuntimeError("The Rust library has not been compiled.")
+            if library_path.exists():
+                return library_path
 
-            return library_path
+            raise RuntimeError(
+                "The Rust modelardb_embedded library was not found, has it been compiled?"
+            )
 
         # Compute the path to the current working directory to locate the library.
         try:
@@ -109,7 +123,7 @@ class Operations:
             void* modelardb_embedded_connect(char* node_url_ptr,
                                              bool is_server_node);
 
-            int modelardb_embedded_close(void* maybe_operations_ptr, 
+            int modelardb_embedded_close(void* maybe_operations_ptr,
                                          bool is_data_folder);
 
             int modelardb_embedded_create(void* maybe_operations_ptr,
@@ -198,7 +212,7 @@ class Operations:
             """
         )
 
-        return ffi.dlopen(library_path)
+        return ffi.dlopen(str(library_path))
 
     # __library is a class variable to ensure the dynamic library's interface is
     # only defined once by ffi.cdef() and that it is loaded by ffi.dlopen() once.
@@ -246,7 +260,7 @@ class Operations:
 
     @classmethod
     def open_s3(
-            cls, endpoint: str, bucket_name: str, access_key_id: str, secret_access_key: str
+        cls, endpoint: str, bucket_name: str, access_key_id: str, secret_access_key: str
     ):
         """Create an :obj:`Operations` data folder that manages data in an object store with a S3-compatible API.
 
@@ -515,13 +529,13 @@ class Operations:
         self.__check_return_code_and_raise_error(return_code)
 
     def read_time_series_table(
-            self,
-            table_name: str,
-            columns: None | list[str] | list[tuple[str, Aggregate]] = None,
-            group_by: None | list[str] = None,
-            start_time: None | datetime | str = None,
-            end_time: None | datetime | str = None,
-            tags: None | dict[str, str] = None,
+        self,
+        table_name: str,
+        columns: None | list[str] | list[tuple[str, Aggregate]] = None,
+        group_by: None | list[str] = None,
+        start_time: None | datetime | str = None,
+        end_time: None | datetime | str = None,
+        tags: None | dict[str, str] = None,
     ) -> RecordBatch:
         """Reads data from the time series table with `table_name` and returns it. The
         remaining parameters optionally specify which subset of the data to
@@ -600,13 +614,13 @@ class Operations:
         return decompressed_batch_ffi.array()
 
     def copy_time_series_table(
-            self,
-            source_table_name: str,
-            target: Self,
-            target_table_name: str,
-            start_time: None | datetime | str = None,
-            end_time: None | datetime | str = None,
-            tags: None | dict[str, str] = None,
+        self,
+        source_table_name: str,
+        target: Self,
+        target_table_name: str,
+        start_time: None | datetime | str = None,
+        end_time: None | datetime | str = None,
+        tags: None | dict[str, str] = None,
     ):
         """Copies data from the time series table with `source_table_name` in `self` to
         the time series table with `target_table_name` in `target`. The remaining
@@ -672,10 +686,10 @@ class Operations:
             return ffi.NULL
 
     def move(
-            self,
-            source_table_name: str,
-            target: Self,
-            target_table_name: str,
+        self,
+        source_table_name: str,
+        target: Self,
+        target_table_name: str,
     ):
         """Moves all data from the table with `source_table_name` in `self`
         to the table with `target_table_name` in `target`.
@@ -745,12 +759,17 @@ class Operations:
             # Convert the retention period to a string to avoid issues with converting an int to a C type that uses
             # an inconsistent amount of bits across platforms and then converting that to a 64-bit integer in Rust.
             # The string is converted directly to an unsigned 64-bit integer in Rust.
-            retention_period_in_seconds_ptr = ffi.new("char[]", bytes(str(retention_period_in_seconds), "UTF-8"))
+            retention_period_in_seconds_ptr = ffi.new(
+                "char[]", bytes(str(retention_period_in_seconds), "UTF-8")
+            )
         else:
             retention_period_in_seconds_ptr = ffi.NULL
 
         return_code = self.__library.modelardb_embedded_vacuum(
-            self.__operations_ptr, self.__is_data_folder, table_name_ptr, retention_period_in_seconds_ptr
+            self.__operations_ptr,
+            self.__is_data_folder,
+            table_name_ptr,
+            retention_period_in_seconds_ptr,
         )
         self.__check_return_code_and_raise_error(return_code)
 
@@ -795,7 +814,7 @@ def open_local(data_folder_path: str) -> Operations:
 
 
 def open_s3(
-        endpoint: str, bucket_name: str, access_key_id: str, secret_access_key: str
+    endpoint: str, bucket_name: str, access_key_id: str, secret_access_key: str
 ) -> Operations:
     """Create an :obj:`Operations` data folder that manages data in an object store with a S3-compatible API.
 
---
+++ crates/modelardb_embedded/bindings/python/setup.py
@@ -0,0 +1,172 @@
+# Copyright 2025 The ModelarDB Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import shutil
+import platform
+from pathlib import Path
+
+from setuptools import setup, find_packages, Extension
+from setuptools.command.sdist import sdist
+from setuptools.command.build_ext import build_ext
+from setuptools.command.bdist_wheel import bdist_wheel
+
+CRATES_FOLDER_NAME = "crates"
+CARGO_BUILD_NAME = "Cargo.toml"
+CARGO_LOCK_NAME = "Cargo.lock"
+
+
+def get_repository_root():
+    """Return the root directory of the repository."""
+    return Path.cwd().parent.parent.parent.parent
+
+
+def ignore_bindings_folder(path, content):
+    """Filter out folders named bindings from shutil.copytree().
+
+
+    :param path: The path of the directory currently being visited.
+    :param content: The contents of the directory currently being visited.
+    """
+    bindings_folder_name = "bindings"
+    if bindings_folder_name in content:
+        return [bindings_folder_name]
+    return []
+
+
+def copy_file_to_cwd(source_folder: Path, file_name: Path):
+    """Copy :attr:`file_name` in :attr:`source_folder` to the current working directory.
+
+
+    :param schema: The folder to copy a file from.
+    :type schema: Path
+    :param schema: The name of the file to copy.
+    :type schema: Path
+    """
+    source_file_path = source_folder / file_name
+    target_file_path = Path.cwd() / file_name
+    shutil.copyfile(source_file_path, target_file_path)
+
+
+def copy_src_if_repository():
+    """Copy the Rust source code to the current directory if in the repository."""
+    cwd = Path.cwd()
+
+    target_crates_folder = cwd / CRATES_FOLDER_NAME
+    if target_crates_folder.exists():
+        return
+
+    repository_root = get_repository_root()
+    git_folder = repository_root / ".git"
+    if git_folder.exists():
+        source_crates_folder = repository_root / CRATES_FOLDER_NAME
+        shutil.copytree(
+            source_crates_folder, target_crates_folder, ignore=ignore_bindings_folder
+        )
+        copy_file_to_cwd(repository_root, CARGO_BUILD_NAME)
+        copy_file_to_cwd(repository_root, CARGO_LOCK_NAME)
+        return
+
+    raise FileNotFoundError("Failed to locate Rust source code.")
+
+
+def delete_src_if_repository():
+    """Delete the Rust source code in the current directory if in the repository."""
+    cwd = Path.cwd()
+
+    crates_folder = cwd / CRATES_FOLDER_NAME
+    cargo_build_file = cwd / CARGO_BUILD_NAME
+    cargo_lock_file = cwd / CARGO_LOCK_NAME
+
+    repository_root = get_repository_root()
+    git_folder = repository_root / ".git"
+    if git_folder.exists():
+        # Errors are ignored to ensure all files are deleted.
+        shutil.rmtree(crates_folder, ignore_errors=True)
+        cargo_build_file.unlink(missing_ok=True)
+        cargo_lock_file.unlink(missing_ok=True)
+
+
+class RustSDist(sdist):
+    def run(self):
+        """Copy the source code, run sdist, and delete the source code."""
+        copy_src_if_repository()
+        super().run()
+        delete_src_if_repository()
+
+
+class RustBDistWheel(bdist_wheel):
+    def run(self):
+        """Copy the source code, run bdist_wheel, and delete the source code."""
+        copy_src_if_repository()
+        super().run()
+        delete_src_if_repository()
+
+    def get_tag(self):
+        """Create the part of the package filename specifying supported platforms."""
+        python, abi, plat = super().get_tag()
+        # modelardb_embedded is a native library, not a Python extension.
+        python, abi = "py3", "none"
+        return python, abi, plat
+
+
+class RustBuildExt(build_ext):
+    def build_extension(self, ext):
+        """Compile modelardb_embedded to a normal native library with the
+        release profile. A CPython extension is not build as CPython's API is
+        not used and an extension may need changes for each CPython version.
+
+        :param ext: Information about the extension setuptools expects.
+        """
+        dependencies = ["cargo", "rustc", "protoc"]
+        for dependency in dependencies:
+            if not shutil.which(dependency):
+                raise FileNotFoundError(
+                    f"Requires {', '.join(dependencies)}. Missing {dependency}."
+                )
+
+        self.spawn(
+            ["cargo", "build", "--package", "modelardb_embedded", "--lib", "--release"]
+        )
+
+        match platform.system():
+            case "Linux" | "FreeBSD":
+                library_path = "target/release/libmodelardb_embedded.so"
+            case "Darwin":
+                library_path = "target/release/libmodelardb_embedded.dylib"
+            case "Windows":
+                library_path = "target/release/modelardb_embedded.dll"
+            case _:
+                raise RuntimeError("Only Linux, FreeBSD, macOS, and Windows are supported.")
+
+        shutil.move(library_path, self.get_ext_fullpath(ext.name))
+
+    def get_ext_filename(self, ext_name):
+        # Removes the CPython part of ext_name as the library is not linked to
+        # CPython and it simplifies the code for loading the library in Python.
+        filename = super().get_ext_filename(ext_name)
+        start = filename.find(".")
+        end = filename.rfind(".")
+        return filename[:start] + filename[end:]
+
+
+setup(
+    packages=find_packages(exclude=("tests")),
+    include_package_data=True,
+    ext_modules=[Extension("modelardb_embedded", sources=[])],
+    cmdclass={
+        "sdist": RustSDist,
+        "bdist_wheel": RustBDistWheel,
+        "build_ext": RustBuildExt,
+    },
+)
