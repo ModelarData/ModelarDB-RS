262f2500f39ea79963913754358bff01c9882e8b
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-12-09 14:08:08 +0100

    Move the actual configuration to a separate type to make serialization easier

--- crates/modelardb_server/src/configuration.rs
+++ crates/modelardb_server/src/configuration.rs
@@ -31,13 +31,11 @@ use crate::ClusterMode;
 use crate::error::{ModelarDbServerError, Result};
 use crate::storage::StorageEngine;
 
-/// Manages the system's configuration and provides functionality for updating the configuration.
+/// The system's configuration. The configuration can be serialized into a `modelardb.toml`
+/// configuration file and deserialized from it. Accessing and modifying the configuration should
+/// only be done through the [`ConfigurationManager`].
 #[derive(Clone, Serialize, Deserialize)]
-pub struct ConfigurationManager {
-    /// The mode of the cluster used to determine the behaviour when starting the server,
-    /// creating tables, updating the remote object store, and querying.
-    #[serde(skip)]
-    pub(crate) cluster_mode: ClusterMode,
+struct Configuration {
     /// Amount of memory to reserve for storing multivariate time series.
     multivariate_reserved_memory_in_bytes: u64,
     /// Amount of memory to reserve for storing uncompressed data buffers.
@@ -59,6 +57,19 @@ pub struct ConfigurationManager {
     pub(crate) writer_threads: u8,
 }
 
+/// Manages the system's configuration and provides functionality for updating the configuration.
+#[derive(Clone)]
+pub struct ConfigurationManager {
+    /// The mode of the cluster used to determine the behaviour when starting the server,
+    /// creating tables, updating the remote object store, and querying.
+    pub(crate) cluster_mode: ClusterMode,
+    /// The local data folder that stores the configuration file at the root.
+    local_data_folder: DataFolder,
+    /// The configuration of the system. This is stored in a separate type to allow for easier
+    /// serialization and deserialization.
+    configuration: Configuration,
+}
+
 impl ConfigurationManager {
     /// Create a new [`ConfigurationManager`] using the `modelardb.toml` configuration file in the
     /// local data folder. If the file does not exist, a configuration file is created with the
@@ -74,13 +85,13 @@ impl ConfigurationManager {
             Ok(conf_file) => {
                 // If the configuration file exists, load the configuration from the file.
                 let bytes = conf_file.bytes().await?;
-                let mut configuration_from_file = toml::from_slice::<Self>(&bytes)?;
+                let configuration_from_file = toml::from_slice::<Configuration>(&bytes)?;
 
-                // The cluster mode is always ClusterMode::SingleNode when loading the configuration
-                // from the file.
-                configuration_from_file.cluster_mode = cluster_mode;
-
-                Self::validate_configuration(configuration_from_file)
+                Self::validate_configuration(
+                    local_data_folder,
+                    cluster_mode,
+                    configuration_from_file,
+                )
             }
             Err(error) => match error {
                 Error::NotFound { .. } => {
@@ -105,8 +116,7 @@ impl ConfigurationManager {
                     let transfer_time_in_seconds = env::var("MODELARDBD_TRANSFER_TIME_IN_SECONDS")
                         .map_or(None, |value| Some(value.parse().unwrap()));
 
-                    let configuration_manager = Self {
-                        cluster_mode,
+                    let configuration = Configuration {
                         multivariate_reserved_memory_in_bytes,
                         uncompressed_reserved_memory_in_bytes,
                         compressed_reserved_memory_in_bytes,
@@ -118,12 +128,12 @@ impl ConfigurationManager {
                     };
 
                     // Create the TOML file.
-                    let toml = toml::to_string(&configuration_manager)?;
+                    let toml = toml::to_string(&configuration)?;
                     object_store
                         .put(conf_file_path, PutPayload::from(toml.into_bytes()))
                         .await?;
 
-                    Self::validate_configuration(configuration_manager)
+                    Self::validate_configuration(local_data_folder, cluster_mode, configuration)
                 }
                 _ => Err(ModelarDbServerError::InvalidState(format!(
                     "Configuration file '{conf_file_path}' cannot be read."
@@ -134,7 +144,11 @@ impl ConfigurationManager {
 
     /// Validate the fields in `configuration` and return the [`ConfigurationManager`] if it is
     /// valid. If the configuration is invalid, return [`ModelarDbServerError`].
-    fn validate_configuration(configuration: Self) -> Result<Self> {
+    fn validate_configuration(
+        local_data_folder: DataFolder,
+        cluster_mode: ClusterMode,
+        configuration: Configuration,
+    ) -> Result<Self> {
         // TODO: Add support for running multiple threads per component. The individual
         //       components in the storage engine have not been validated with multiple threads, e.g.,
         //       UncompressedDataManager may have race conditions finishing buffers if multiple
@@ -147,12 +161,16 @@ impl ConfigurationManager {
                 "Only one thread per component is currently supported.".to_string(),
             ))
         } else {
-            Ok(configuration)
+            Ok(Self {
+                cluster_mode,
+                local_data_folder,
+                configuration,
+            })
         }
     }
 
     pub(crate) fn multivariate_reserved_memory_in_bytes(&self) -> u64 {
-        self.multivariate_reserved_memory_in_bytes
+        self.configuration.multivariate_reserved_memory_in_bytes
     }
 
     /// Set the new value and update the amount of memory for multivariate data in the storage engine.
@@ -164,7 +182,7 @@ impl ConfigurationManager {
         // Since the storage engine only keeps track of the remaining reserved memory, calculate
         // how much the value should change.
         let value_change = new_multivariate_reserved_memory_in_bytes as i64
-            - self.multivariate_reserved_memory_in_bytes as i64;
+            - self.configuration.multivariate_reserved_memory_in_bytes as i64;
 
         storage_engine
             .write()
@@ -172,11 +190,12 @@ impl ConfigurationManager {
             .adjust_multivariate_remaining_memory_in_bytes(value_change)
             .await;
 
-        self.multivariate_reserved_memory_in_bytes = new_multivariate_reserved_memory_in_bytes;
+        self.configuration.multivariate_reserved_memory_in_bytes =
+            new_multivariate_reserved_memory_in_bytes;
     }
 
     pub(crate) fn uncompressed_reserved_memory_in_bytes(&self) -> u64 {
-        self.uncompressed_reserved_memory_in_bytes
+        self.configuration.uncompressed_reserved_memory_in_bytes
     }
 
     /// Set the new value and update the amount of memory for uncompressed data in the storage
@@ -190,7 +209,7 @@ impl ConfigurationManager {
         // Since the storage engine only keeps track of the remaining reserved memory, calculate
         // how much the value should change.
         let value_change = new_uncompressed_reserved_memory_in_bytes as i64
-            - self.uncompressed_reserved_memory_in_bytes as i64;
+            - self.configuration.uncompressed_reserved_memory_in_bytes as i64;
 
         storage_engine
             .write()
@@ -198,13 +217,14 @@ impl ConfigurationManager {
             .adjust_uncompressed_remaining_memory_in_bytes(value_change)
             .await?;
 
-        self.uncompressed_reserved_memory_in_bytes = new_uncompressed_reserved_memory_in_bytes;
+        self.configuration.uncompressed_reserved_memory_in_bytes =
+            new_uncompressed_reserved_memory_in_bytes;
 
         Ok(())
     }
 
     pub(crate) fn compressed_reserved_memory_in_bytes(&self) -> u64 {
-        self.compressed_reserved_memory_in_bytes
+        self.configuration.compressed_reserved_memory_in_bytes
     }
 
     /// Set the new value and update the amount of memory for compressed data in the storage engine.
@@ -217,7 +237,7 @@ impl ConfigurationManager {
         // Since the storage engine only keeps track of the remaining reserved memory, calculate
         // how much the value should change.
         let value_change = new_compressed_reserved_memory_in_bytes as i64
-            - self.compressed_reserved_memory_in_bytes as i64;
+            - self.configuration.compressed_reserved_memory_in_bytes as i64;
 
         storage_engine
             .write()
@@ -225,13 +245,14 @@ impl ConfigurationManager {
             .adjust_compressed_remaining_memory_in_bytes(value_change)
             .await?;
 
-        self.compressed_reserved_memory_in_bytes = new_compressed_reserved_memory_in_bytes;
+        self.configuration.compressed_reserved_memory_in_bytes =
+            new_compressed_reserved_memory_in_bytes;
 
         Ok(())
     }
 
     pub(crate) fn transfer_batch_size_in_bytes(&self) -> Option<u64> {
-        self.transfer_batch_size_in_bytes
+        self.configuration.transfer_batch_size_in_bytes
     }
 
     /// Set the new value and update the transfer batch size in the storage engine. If the value was
@@ -247,13 +268,13 @@ impl ConfigurationManager {
             .set_transfer_batch_size_in_bytes(new_transfer_batch_size_in_bytes)
             .await?;
 
-        self.transfer_batch_size_in_bytes = new_transfer_batch_size_in_bytes;
+        self.configuration.transfer_batch_size_in_bytes = new_transfer_batch_size_in_bytes;
 
         Ok(())
     }
 
     pub(crate) fn transfer_time_in_seconds(&self) -> Option<u64> {
-        self.transfer_time_in_seconds
+        self.configuration.transfer_time_in_seconds
     }
 
     /// Set the new value and update the transfer time in the storage engine. If the value was
@@ -269,23 +290,41 @@ impl ConfigurationManager {
             .set_transfer_time_in_seconds(new_transfer_time_in_seconds)
             .await?;
 
-        self.transfer_time_in_seconds = new_transfer_time_in_seconds;
+        self.configuration.transfer_time_in_seconds = new_transfer_time_in_seconds;
 
         Ok(())
+    }
+
+    pub(crate) fn ingestion_threads(&self) -> u8 {
+        self.configuration.ingestion_threads
+    }
+
+    pub(crate) fn compression_threads(&self) -> u8 {
+        self.configuration.compression_threads
+    }
+
+    pub(crate) fn writer_threads(&self) -> u8 {
+        self.configuration.writer_threads
     }
 
     /// Encode the current configuration into a [`Configuration`](protocol::Configuration)
     /// protobuf message and serialize it.
     pub(crate) fn encode_and_serialize(&self) -> Vec<u8> {
         let configuration = protocol::Configuration {
-            multivariate_reserved_memory_in_bytes: self.multivariate_reserved_memory_in_bytes,
-            uncompressed_reserved_memory_in_bytes: self.uncompressed_reserved_memory_in_bytes,
-            compressed_reserved_memory_in_bytes: self.compressed_reserved_memory_in_bytes,
-            transfer_batch_size_in_bytes: self.transfer_batch_size_in_bytes,
-            transfer_time_in_seconds: self.transfer_time_in_seconds,
-            ingestion_threads: self.ingestion_threads as u32,
-            compression_threads: self.compression_threads as u32,
-            writer_threads: self.writer_threads as u32,
+            multivariate_reserved_memory_in_bytes: self
+                .configuration
+                .multivariate_reserved_memory_in_bytes,
+            uncompressed_reserved_memory_in_bytes: self
+                .configuration
+                .uncompressed_reserved_memory_in_bytes,
+            compressed_reserved_memory_in_bytes: self
+                .configuration
+                .compressed_reserved_memory_in_bytes,
+            transfer_batch_size_in_bytes: self.configuration.transfer_batch_size_in_bytes,
+            transfer_time_in_seconds: self.configuration.transfer_time_in_seconds,
+            ingestion_threads: self.configuration.ingestion_threads as u32,
+            compression_threads: self.configuration.compression_threads as u32,
+            writer_threads: self.configuration.writer_threads as u32,
         };
 
         configuration.encode_to_vec()
