f17390e1fa0f240473bcf6a8264b4610813f8d4e
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-12-14 17:50:31 +0100

    Add try_new method to WriteAheadLog

--- crates/modelardb_storage/src/write_ahead_log.rs
+++ crates/modelardb_storage/src/write_ahead_log.rs
@@ -13,16 +13,42 @@
  * limitations under the License.
  */
 
-//! Implementation of types that provide a write-ahead-log for ModelarDB that can be used to
+//! Implementation of types that provide a write-ahead log for ModelarDB that can be used to
 //! efficiently persist data and operations on disk to avoid data loss and enable crash recovery.
 
 use std::collections::HashMap;
 use std::fs::File;
+use std::path::PathBuf;
 
-/// Write-ahead-log that logs data on a per-table level and operations separately.
-struct WriteAheadLog {
+use crate::WRITE_AHEAD_LOG_FOLDER;
+use crate::data_folder::DataFolder;
+use crate::error::Result;
+
+
+/// Write-ahead log that logs data on a per-table level and operations separately.
+pub struct WriteAheadLog {
+    log_folder_path: PathBuf,
     table_logs: HashMap<String, WriteAheadLogFile>,
     operation_log: WriteAheadLogFile,
+}
+
+impl WriteAheadLog {
+    /// Create a new [`WriteAheadLog`] that stores the log in the root of `local_data_folder` in
+    /// the [`WRITE_AHEAD_LOG_FOLDER`] folder. If the folder does not exist, it is created. If the
+    /// log could not be created, return [`ModelarDbStorageError`](crate::error::ModelarDbStorageError).
+    pub fn try_new(local_data_folder: DataFolder) -> Result<Self> {
+        // Create the folder for the write-ahead log if it does not exist.
+        let location = local_data_folder.location();
+        let log_folder_path = PathBuf::from(format!("{location}/{WRITE_AHEAD_LOG_FOLDER}"));
+
+        std::fs::create_dir(log_folder_path.clone())?;
+
+        Ok(Self {
+            log_folder_path: log_folder_path.clone(),
+            table_logs: HashMap::new(),
+            operation_log: WriteAheadLogFile::try_new(log_folder_path.join("operations.wal"))?,
+        })
+    }
 }
 
 /// Wrapper around a [`File`] that enforces that [`sync_all()`](File::sync_all) is called
