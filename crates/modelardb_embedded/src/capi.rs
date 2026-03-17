fd777ed1493f693e1ea61814cea3d21c73f4bbfc
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2026-03-17 10:49:05 +0100

    Fix inconsistent naming in doc comments

--- crates/modelardb_embedded/src/capi.rs
+++ crates/modelardb_embedded/src/capi.rs
@@ -289,15 +289,15 @@ unsafe fn modelardb_type(
 }
 
 /// Creates a table with the name in `table_name_ptr`, the schema in `schema_ptr`, and the error
-/// bounds in `error_bounds_ptr` in the [`DataFolder`] or [`Client`] in `maybe_operations_ptr`.
+/// bounds in `error_bound_array_ptr` in the [`DataFolder`] or [`Client`] in `maybe_operations_ptr`.
 /// Assumes `maybe_operations_ptr` points to a [`DataFolder`] or [`Client`]; `table_name_ptr` points
 /// to a valid C string; `schema_ptr` points to an Apache Arrow [`Schema`]; `error_bound_array_ptr`
 /// and `error_bound_array_schema_ptr` point to a [`MapArray`] that maps from field column names to
 /// error bounds; and `generated_columns_array_ptr` and `generated_columns_array_schema_ptr` point
-/// to a [`MapArray`] that maps from field column names to error bounds. If an error bound is zero
-/// the column with that name is stored losslessly, if the error bound is positive it is interpreted
-/// as an absolute error bound, and if the error bound is negative it is interpreted as a relative
-/// error bound.
+/// to a [`MapArray`] that maps from field column names to generated columns. If an error bound is
+/// zero the column with that name is stored losslessly, if the error bound is positive it is
+/// interpreted as an absolute error bound, and if the error bound is negative it is interpreted as
+/// a relative error bound.
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn modelardb_embedded_create(
     maybe_operations_ptr: *mut c_void,
@@ -402,7 +402,7 @@ fn error_bounds_array_to_error_bounds(
 
 /// Writes the name of all tables to `tables_array_ptr` and `tables_array_schema_ptr` in the
 /// [`DataFolder`] or [`Client`] in `maybe_operations_ptr`. Assumes `maybe_operations_ptr` points to
-/// a [`DataFolder`] or [`Client`]; `table_array_ptr` is a valid pointer to enough memory for an
+/// a [`DataFolder`] or [`Client`]; `tables_array_ptr` is a valid pointer to enough memory for an
 /// Apache Arrow C Data Interface Array; and `tables_array_schema_ptr` is a valid pointer to enough
 /// memory for an Apache Arrow C Data Interface Schema.
 #[unsafe(no_mangle)]
@@ -499,9 +499,10 @@ unsafe fn schema(
 /// Writes the data in `uncompressed_struct_array_ptr` and `uncompressed_struct_array_schema_ptr`
 /// to the table with the table name in `table_name_ptr` in the [`DataFolder`] or [`Client`] in
 /// `maybe_operations_ptr`. Assumes `maybe_operations_ptr` points to a [`DataFolder`] or [`Client`];
-/// `table_name_ptr` points to a valid C string; `struct_array_ptr` is a valid pointer to enough
-/// memory for an Apache Arrow C Data Interface Array; and `struct_array_schema_ptr` is a valid
-/// pointer to enough memory for an Apache Arrow C Data Interface Schema.
+/// `table_name_ptr` points to a valid C string; `uncompressed_struct_array_ptr` is a valid pointer
+/// to enough memory for an Apache Arrow C Data Interface Array; and
+/// `uncompressed_struct_array_schema_ptr` is a valid pointer to enough memory for an Apache Arrow C
+/// Data Interface Schema.
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn modelardb_embedded_write(
     maybe_operations_ptr: *mut c_void,
