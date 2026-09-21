13286eb8b6ec8a67b830f2f8c4288476eb84fa9c
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2026-09-21 08:58:41 +0200

    Simplify initialization of S3 Object Store in DataFolder

--- crates/modelardb_storage/src/data_folder/mod.rs
+++ crates/modelardb_storage/src/data_folder/mod.rs
@@ -199,24 +199,13 @@ impl DataFolder {
             ("aws_secret_access_key".to_owned(), secret_access_key),
             ("aws_endpoint_url".to_owned(), endpoint),
             ("aws_bucket_name".to_owned(), bucket_name),
+            ("aws_allow_http".to_owned(), "true".to_owned()),
         ]);
 
         let url = Url::parse(&location)
             .map_err(|error| ModelarDbStorageError::InvalidArgument(error.to_string()))?;
 
-        // Build the Amazon S3 object store with the given storage options manually to allow http.
-        let object_store = storage_options
-            .iter()
-            .fold(
-                AmazonS3Builder::new()
-                    .with_url(url.to_string())
-                    .with_allow_http(true),
-                |builder, (key, value)| {
-                    let key = key.parse().unwrap();
-                    builder.with_config(key, value)
-                },
-            )
-            .build()?;
+        let (object_store, _path) = object_store::parse_url_opts(&url, &storage_options)?;
 
         Self::try_new(location, storage_options, Arc::new(object_store)).await
     }
@@ -240,8 +229,10 @@ impl DataFolder {
             ("azure_container_name".to_owned(), container_name),
             ("azure_storage_use_emulator".to_owned(), use_emulator),
         ]);
+
         let url = Url::parse(&location)
             .map_err(|error| ModelarDbStorageError::InvalidArgument(error.to_string()))?;
+
         let (object_store, _path) = object_store::parse_url_opts(&url, &storage_options)?;
 
         Self::try_new(location, storage_options, Arc::new(object_store)).await
